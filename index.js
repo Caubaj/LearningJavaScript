8496fdaff73fcb8b55218dd8f73165c56cd2b786
Carlos Portillo <carlosportillo7890@gmail.com>
2025-01-14 09:26:01 -0800

    Learning Sorting and Date Constants

---
+++ index.js
@@ -0,0 +1,41 @@
+// Sort() = method used to sort eleemts of an array in place. 
+
+
+// let fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+
+// console.log(fruits)
+
+
+// let nums = [1,5, 3, 67, 8, 9, 0];
+
+// nums.sort((a, b) => b - a);
+
+// console.log(nums);
+
+// const people = [{name: "spange", age: 30, gpa: 3.0},
+//                 {name: "patricio", age: 37, gpa: 1.3},
+//                 {name: "sandy", age: 27, gpa: 4.0}]
+
+
+// people.sort((a, b) => a.name.localeCompare(b.name));
+
+// console.log(people);
+
+
+//DATES 
+
+
+// const date1 = new Date("2023-12-31");
+// const date2 = new Date("2021-01-01");
+
+// if(date1 < date2)
+// {
+//     console.log("Happy New Year!")
+// }
+// else{
+//     console.log("No Happy New Year")
+// }
+
+
+
+// console.log();
